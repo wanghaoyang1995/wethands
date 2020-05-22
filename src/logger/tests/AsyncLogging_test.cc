5eca8a04d5b70edb40aa89239add984f2916d590
ggggitfkbjg <767105779@qq.com>
2020-05-22 14:58:59 +0800

    update AsyncLogging_test.cc

--- src/logger/tests/AsyncLogging_test.cc
+++ src/logger/tests/AsyncLogging_test.cc
@@ -1,18 +1,29 @@
 #include "src/logger/AsyncLogging.h"
 #include "src/logger/Logger.h"
+#include "src/thread/CurrentThread.h"
 
 using namespace wethands;
 
+AsyncLogging* gAsyncLog = nullptr;
+
+void output(const char* line, size_t len) {
+  gAsyncLog->Append(line, len);
+}
+
 void test() {
-  for (int i = 0; i < 99999; ++i) {
-    LOG_INFO << "log " << i;
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 99999; ++j) {
+      LOG_INFO << "log " << j;
+    }
+    CurrentThread::SleepUsec(600000);
   }
 }
 
 int main() {
-  AsyncLogging asynclogging("asynclog", 1024*2);
-
-  asynclogging.Start();
+  AsyncLogging asynclog("asynclog", 1024*2);
+  gAsyncLog = &asynclog;
+  Logger::setOutputFunc(output);
+  asynclog.Start();
   test();
   return 0;
 }
